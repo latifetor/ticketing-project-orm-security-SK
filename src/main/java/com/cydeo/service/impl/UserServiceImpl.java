53eb29350395872090dd7702b2493b1cde7d0334
supawadeek <latifetor@gmail.com>
2023-12-04 12:30:05 -0500

    Ticketing-Project-ORM Part-2

--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -40,6 +40,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public void save(UserDTO dto) {
 
+        userRepository.save(userMapper.convertToEntity(dto));
     }
 
     @Override
