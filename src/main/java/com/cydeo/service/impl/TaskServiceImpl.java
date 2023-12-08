64d913cd15aaf414d5641d35749b9945f60c6c2c
supawadeek <latifetor@gmail.com>
2023-12-08 13:31:43 -0500

    Ticketing-Project-ORM Part-4

--- src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
@@ -1,13 +1,25 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.TaskDTO;
+import com.cydeo.entity.Task;
+import com.cydeo.mapper.TaskMapper;
+import com.cydeo.repository.TaskRepository;
 import com.cydeo.service.TaskService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TaskServiceImpl implements TaskService {
+
+    TaskRepository taskRepository;
+    TaskMapper taskMapper;
+
+    public TaskServiceImpl(TaskRepository taskRepository, TaskMapper taskMapper) {
+        this.taskRepository = taskRepository;
+        this.taskMapper = taskMapper;
+    }
 
     @Override
     public TaskDTO findById(Long id) {
@@ -16,11 +28,13 @@ public class TaskServiceImpl implements TaskService {
 
     @Override
     public List<TaskDTO> listAllTasks() {
-        return null;
+        List<Task> list = taskRepository.findAll();
+        return list.stream().map(taskMapper::convertToDTO).collect(Collectors.toList());
     }
 
     @Override
     public void save(TaskDTO dto) {
+
 
     }
 
